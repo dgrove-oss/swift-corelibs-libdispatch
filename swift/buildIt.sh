e4f4c5ca154d4f798463d613b3e870c8fd165930
David Grove <groved@us.ibm.com>
2016-02-01 23:21:00 +0000

    update WIP script

--- swift/buildIt.sh
+++ swift/buildIt.sh
@@ -2,14 +2,11 @@
 
 cd Dispatch
 
-# helper functions for decls the swiftc importer doesn't handle
-clang -c -fblocks Dispatch.c -o Dispatch.o
-
 # make Dispatch.swiftmodule
-$SWIFT_ROOT/bin/swiftc -parse-as-library -I . -emit-module -emit-library -emit-module-path . -Xcc -fblocks -Xlinker Dispatch.o Dispatch.swift -o libDispatchWrapper.so
+$SWIFT_ROOT/bin/swiftc -parse-as-library -I . -emit-module -emit-module-path . -Xcc -fblocks -Xlinker -ldispatch Dispatch.swift
 
 cd ../samples
 
 # compile a test program
-$SWIFT_ROOT/bin/swiftc -I ../Dispatch -Xcc -fblocks -Xlinker -lBlocksRuntime -Xlinker -L../Dispatch -Xlinker -lDispatchWrapper average.swift
+$SWIFT_ROOT/bin/swiftc -I ../Dispatch -Xcc -fblocks -Xlinker -lBlocksRuntime -Xlinker -ldispatch average.swift
 
