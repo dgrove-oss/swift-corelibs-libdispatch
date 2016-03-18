5d63681d76b7cb8f6209be61154ed0b1f4c0dadd
David Grove <groved@us.ibm.com>
2016-03-18 20:02:11 +0000

    completed initial pass at full overlay

--- src/swift/Dispatch.swift
+++ src/swift/Dispatch.swift
@@ -25,9 +25,10 @@ import CDispatch
 //   a change to undertake at this stage of development.
 //===----------------------------------------------------------------------===//
 
-
+//////////
 // Define Swift-level class hierarchy for subset of dispatch types
 // returned from API methods with DISPATCH_RETURNS_RETAINED
+//////////
 
 public typealias dispatch_object_t = DispatchObject
 public class DispatchObject {
@@ -105,7 +106,7 @@ public func dispatch_walltime(when:UnsafePointer<timespec>, _ delta:Int64) -> di
 // object.h
 //////////
 
-// dispatch_retain/dispatch_release intentionally suppressed.
+// dispatch_retain/dispatch_release intentionally not exposed.
 
 public func dispatch_get_context(object:dispatch_object_t) -> UnsafeMutablePointer<Void> {
   return CDispatch.dispatch_get_context(_to_dot(object.cobj))
@@ -116,7 +117,7 @@ public func dispatch_set_context(object:dispatch_object_t, _ context:UnsafeMutab
 }
 
 public func dispatch_set_finalizer_f(object:dispatch_object_t, 
-		                             _ finalizer:dispatch_function_t) -> Void {
+                                     _ finalizer:dispatch_function_t) -> Void {
   CDispatch.dispatch_set_finalizer_f(_to_dot(object.cobj), finalizer)
 }
 
@@ -143,8 +144,8 @@ public func dispatch_sync(queue:dispatch_queue_t, _ block:dispatch_block_t) -> V
 public typealias dispatch_apply_block_t = @convention(block) (size_t) -> Void
 
 public func dispatch_apply(iterations:Int, _ queue:dispatch_queue_t,
-		                   _ block:dispatch_apply_block_t) -> Void {
-  CDispatch.dispatch_apply(iterations, queue.cobj, block)						   
+                           _ block:dispatch_apply_block_t) -> Void {
+  CDispatch.dispatch_apply(iterations, queue.cobj, block)
 }
 
 // skip deprecated function dispatch_get_current_queue
@@ -169,8 +170,6 @@ public var DISPATCH_QUEUE_PRIORITY_BACKGROUND: dispatch_queue_priority_t {
   return -32768
 }
 
-
-
 public func dispatch_get_global_queue(identifier:Int, _ flags:UInt) -> dispatch_queue_t {
   return DispatchQueue(CDispatch.dispatch_get_global_queue(identifier, flags))
 }
@@ -187,7 +186,7 @@ public var DISPATCH_QUEUE_CONCURRENT: dispatch_queue_attr_t {
 }
 
 public func dispatch_queue_create(label:UnsafePointer<Int8>,
-								  _ attr:dispatch_queue_attr_t) -> dispatch_queue_t {
+                                  _ attr:dispatch_queue_attr_t) -> dispatch_queue_t {
   return DispatchQueue(CDispatch.dispatch_queue_create(label, attr))
 }
 
@@ -214,7 +213,7 @@ public func dispatch_main() -> Void {
 }
 
 public func dispatch_after(when:dispatch_time_t, _ queue:dispatch_queue_t,
-	                       _ block:dispatch_block_t) -> Void {
+                           _ block:dispatch_block_t) -> Void {
   CDispatch.dispatch_after(when, queue.cobj, block)
 }
 
@@ -228,8 +227,8 @@ public func dispatch_barrier_sync(queue:dispatch_queue_t, _ block:dispatch_block
 
 public func dispatch_queue_set_specific(queue:dispatch_queue_t,
                                         _ key:UnsafeMutablePointer<Void>,
-	                                    _ context:UnsafeMutablePointer<Void>,
-										_ destructor:dispatch_function_t) -> Void {
+                                        _ context:UnsafeMutablePointer<Void>,
+                                        _ destructor:dispatch_function_t) -> Void {
   CDispatch.dispatch_queue_set_specific(queue.cobj, key, context, destructor)
 }
 
@@ -283,7 +282,7 @@ public func dispatch_block_create(flags:dispatch_block_flags_t,
 }  
 
 // skip dispatch_block_create_with_qos_class; no QoS on Linux
-		
+        
 public func dispatch_block_perform(flags:dispatch_block_flags_t, _ block:dispatch_block_t) -> Void {
   CDispatch.dispatch_block_perform(flags, block)
 }
@@ -293,8 +292,8 @@ public func dispatch_block_wait(block:dispatch_block_t, _ timeout:dispatch_time_
 }
 
 public func dispatch_block_notify(block:dispatch_block_t, _ queue:dispatch_queue_t,
-		                          _ notification_block:dispatch_block_t) -> Void {
-  return CDispatch.dispatch_block_notify(block, queue.cobj, notification_block)		
+                                  _ notification_block:dispatch_block_t) -> Void {
+  return CDispatch.dispatch_block_notify(block, queue.cobj, notification_block)        
 }
 
 public func dispatch_block_cancel(block:dispatch_block_t) -> Void {
@@ -306,8 +305,107 @@ public func dispatch_block_testcancel(block:dispatch_block_t) -> Int {
 }
 
 //////////
-// TODO: source.h
+// source.h
 //////////
+
+public typealias dispatch_source_type_t = COpaquePointer
+
+public var DISPATCH_SOURCE_TYPE_DATA_ADD: dispatch_source_type_t {
+  return _swift_dispatch_source_type_data_add()
+}
+public var DISPATCH_SOURCE_TYPE_DATA_OR: dispatch_source_type_t {
+  return _swift_dispatch_source_type_data_or()
+}
+public var DISPATCH_SOURCE_TYPE_READ: dispatch_source_type_t {
+  return _swift_dispatch_source_type_read()
+}
+public var DISPATCH_SOURCE_TYPE_PROC: dispatch_source_type_t {
+  return _swift_dispatch_source_type_proc()
+}
+public var DISPATCH_SOURCE_TYPE_SIGNAL: dispatch_source_type_t {
+  return _swift_dispatch_source_type_signal()
+}
+public var DISPATCH_SOURCE_TYPE_TIMER: dispatch_source_type_t {
+  return _swift_dispatch_source_type_timer()
+}
+public var DISPATCH_SOURCE_TYPE_VNODE: dispatch_source_type_t {
+  return _swift_dispatch_source_type_vnode()
+}
+public var DISPATCH_SOURCE_TYPE_WRITE: dispatch_source_type_t {
+  return _swift_dispatch_source_type_write()
+}
+
+public typealias dispatch_source_proc_flags_t = CDispatch.dispatch_source_proc_flags_t
+public var DISPATCH_PROC_EXIT: dispatch_source_proc_flags_t { return 0x80000000 }
+public var DISPATCH_PROC_FORK: dispatch_source_proc_flags_t { return 0x40000000 }
+public var DISPATCH_PROC_EXEC: dispatch_source_proc_flags_t { return 0x20000000 }
+public var DISPATCH_PROC_SIGNAL: dispatch_source_proc_flags_t { return 0x08000000 }
+
+public typealias dispatch_source_vnode_flags_t = CDispatch.dispatch_source_vnode_flags_t
+public var DISPATCH_VNODE_DELETE: dispatch_source_vnode_flags_t { return 0x1 }
+public var DISPATCH_VNODE_WRITE:  dispatch_source_vnode_flags_t { return 0x2 }
+public var DISPATCH_VNODE_EXTEND: dispatch_source_vnode_flags_t { return 0x4 }
+public var DISPATCH_VNODE_ATTRIB: dispatch_source_vnode_flags_t { return 0x8 }
+public var DISPATCH_VNODE_LINK:   dispatch_source_vnode_flags_t { return 0x10 }
+public var DISPATCH_VNODE_RENAME: dispatch_source_vnode_flags_t { return 0x20 }
+public var DISPATCH_VNODE_REVOKE: dispatch_source_vnode_flags_t { return 0x40 }
+
+public typealias dispatch_source_timer_flags_t = CDispatch.dispatch_source_timer_flags_t
+public var DISPATCH_TIMER_STRICT: dispatch_source_timer_flags_t { return 1 }
+
+public func dispatch_source_create(type:dispatch_source_type_t,
+                                   _ handle:UInt,
+                                   _ mask:UInt,
+                                   _ queue:dispatch_queue_t?) -> dispatch_source_t {
+  let cqueue = queue != nil ? queue!.cobj : nil
+  return DispatchSource(CDispatch.dispatch_source_create(type, handle, mask, cqueue))
+}
+
+public func dispatch_source_set_event_handler(source:dispatch_source_t,
+                                              _ handler:dispatch_block_t) -> Void {
+  CDispatch.dispatch_source_set_event_handler(source.cobj, handler)
+}
+
+public func dispatch_source_set_cancel_handler(source:dispatch_source_t,
+                                               _ handler:dispatch_block_t) -> Void {
+  CDispatch.dispatch_source_set_cancel_handler(source.cobj, handler)
+}
+
+public func dispatch_source_cancel(source:dispatch_source_t) -> Void {
+  CDispatch.dispatch_source_cancel(source.cobj)
+}
+
+public func dispatch_source_testcancel(source:dispatch_source_t) -> Void {
+  CDispatch.dispatch_source_testcancel(source.cobj)
+}
+
+public func dispatch_source_get_handle(source:dispatch_source_t) -> UInt {
+  return CDispatch.dispatch_source_get_handle(source.cobj)
+}
+
+public func dispatch_source_get_mask(source:dispatch_source_t) -> UInt {
+  return CDispatch.dispatch_source_get_mask(source.cobj)
+}
+
+public func dispatch_source_get_data(source:dispatch_source_t) -> UInt {
+  return CDispatch.dispatch_source_get_data(source.cobj)
+}
+
+public func dispatch_source_merge_data(source:dispatch_source_t,_ value:UInt) -> Void {
+  CDispatch.dispatch_source_merge_data(source.cobj, value)
+}
+
+public func dispatch_source_set_timer(source:dispatch_source_t,
+                                      _ start:dispatch_time_t,
+                                      _ interval:UInt64,
+                                      _ leeway:UInt64) -> Void {
+  CDispatch.dispatch_source_set_timer(source.cobj, start, interval, leeway)
+}
+
+public func dispatch_source_set_registration_handler(source:dispatch_source_t,
+                                                     _ handler:dispatch_block_t) -> Void {
+  CDispatch.dispatch_source_set_registration_handler(source.cobj, handler)
+}
 
 //////////
 // group.h
@@ -318,7 +416,7 @@ public func dispatch_group_create() -> dispatch_group_t {
 }
 
 public func dispatch_group_async(group:dispatch_group_t, _ queue:dispatch_queue_t,
-	                             _ block:dispatch_block_t) -> Void {
+                                 _ block:dispatch_block_t) -> Void {
   CDispatch.dispatch_group_async(group.cobj, queue.cobj, block)
 }
 
@@ -327,7 +425,7 @@ public func dispatch_group_wait(group:dispatch_group_t, _ timeout:dispatch_time_
 }
 
 public func dispatch_group_notify(group:dispatch_group_t, _ queue:dispatch_queue_t,
-	                              _ block:dispatch_block_t) -> Void {
+                                  _ block:dispatch_block_t) -> Void {
   CDispatch.dispatch_group_notify(group.cobj, queue.cobj, block)
 }
 
@@ -360,7 +458,7 @@ public func dispatch_semaphore_signal(dsema:dispatch_semaphore_t) -> Int {
 // once.h
 //////////
 
-// skip dispatch_once for now (does a long* predicate really fit that well for Swift?)
+// TODO: skip dispatch_once (does a long* predicate really make sense for Swift?)
 
 //////////
 // data.h
@@ -371,7 +469,7 @@ public var dispatch_data_empty: dispatch_data_t {
 }
 
 public func dispatch_data_create(buffer:UnsafePointer<Void>, _ size:size_t,
-                                 _ queue:dispatch_queue_t,	_ destructor:dispatch_block_t?) -> dispatch_data_t {
+                                 _ queue:dispatch_queue_t,    _ destructor:dispatch_block_t?) -> dispatch_data_t {
   return DispatchData(CDispatch.dispatch_data_create(buffer, size, queue.cobj, destructor))
 }
 
@@ -381,7 +479,7 @@ public func dispatch_data_get_size(data:dispatch_data_t)-> size_t {
 
 public func dispatch_data_create_map(data:dispatch_data_t,
                                      _ buffer_ptr:UnsafeMutablePointer<UnsafePointer<Void>>,
-	                                 _ size_ptr:UnsafeMutablePointer<size_t>) -> dispatch_data_t {
+                                     _ size_ptr:UnsafeMutablePointer<size_t>) -> dispatch_data_t {
   return DispatchData(CDispatch.dispatch_data_create_map(data.cobj, buffer_ptr, size_ptr))
 }
 
@@ -426,15 +524,15 @@ public typealias dispatch_io_handler_block_t = @convention(block) (Int32) -> Voi
 public func dispatch_io_create(type:dispatch_io_type_t,
                                _ fd:dispatch_fd_t,
                                _ queue:dispatch_queue_t,
-	                           _ cleanup_handler:dispatch_io_handler_block_t) -> dispatch_io_t {
+                               _ cleanup_handler:dispatch_io_handler_block_t) -> dispatch_io_t {
   return DispatchIO(CDispatch.dispatch_io_create(type, fd, queue.cobj, cleanup_handler))
-}	
+}    
 
 public func dispatch_io_create_with_path(type:dispatch_io_type_t,
-	                                     _ path:UnsafePointer<Int8>,
-										 _ oflag:Int32, _ mode:mode_t,
+                                         _ path:UnsafePointer<Int8>,
+                                         _ oflag:Int32, _ mode:mode_t,
                                          _ queue:dispatch_queue_t,
-	                                     _ cleanup_handler:dispatch_io_handler_block_t) -> dispatch_io_t {
+                                         _ cleanup_handler:dispatch_io_handler_block_t) -> dispatch_io_t {
   return DispatchIO(CDispatch.dispatch_io_create_with_path(type, path, oflag, mode, queue.cobj, cleanup_handler))
 }
 
@@ -547,42 +645,3 @@ internal func _swift_dispatch_source_type_write() -> dispatch_source_type_t
 
 // dispatch/source.h
 // FIXME: DISPATCH_SOURCE_TYPE_*
-public var DISPATCH_PROC_EXIT: dispatch_source_proc_flags_t {
-  return 0x80000000
-}
-public var DISPATCH_PROC_FORK: dispatch_source_proc_flags_t { return 0x40000000 }
-public var DISPATCH_PROC_EXEC: dispatch_source_proc_flags_t { return 0x20000000 }
-public var DISPATCH_PROC_SIGNAL: dispatch_source_proc_flags_t { return 0x08000000 }
-public var DISPATCH_VNODE_DELETE: dispatch_source_vnode_flags_t { return 0x1 }
-public var DISPATCH_VNODE_WRITE:  dispatch_source_vnode_flags_t { return 0x2 }
-public var DISPATCH_VNODE_EXTEND: dispatch_source_vnode_flags_t { return 0x4 }
-public var DISPATCH_VNODE_ATTRIB: dispatch_source_vnode_flags_t { return 0x8 }
-public var DISPATCH_VNODE_LINK:   dispatch_source_vnode_flags_t { return 0x10 }
-public var DISPATCH_VNODE_RENAME: dispatch_source_vnode_flags_t { return 0x20 }
-public var DISPATCH_VNODE_REVOKE: dispatch_source_vnode_flags_t { return 0x40 }
-public var DISPATCH_TIMER_STRICT: dispatch_source_timer_flags_t { return 1 }
-
-public var DISPATCH_SOURCE_TYPE_DATA_ADD: dispatch_source_type_t {
-  return _swift_dispatch_source_type_data_add()
-}
-public var DISPATCH_SOURCE_TYPE_DATA_OR: dispatch_source_type_t {
-  return _swift_dispatch_source_type_data_or()
-}
-public var DISPATCH_SOURCE_TYPE_READ: dispatch_source_type_t {
-  return _swift_dispatch_source_type_read()
-}
-public var DISPATCH_SOURCE_TYPE_PROC: dispatch_source_type_t {
-  return _swift_dispatch_source_type_proc()
-}
-public var DISPATCH_SOURCE_TYPE_SIGNAL: dispatch_source_type_t {
-  return _swift_dispatch_source_type_signal()
-}
-public var DISPATCH_SOURCE_TYPE_TIMER: dispatch_source_type_t {
-  return _swift_dispatch_source_type_timer()
-}
-public var DISPATCH_SOURCE_TYPE_VNODE: dispatch_source_type_t {
-  return _swift_dispatch_source_type_vnode()
-}
-public var DISPATCH_SOURCE_TYPE_WRITE: dispatch_source_type_t {
-  return _swift_dispatch_source_type_write()
-}
